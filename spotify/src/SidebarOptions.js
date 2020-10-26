7fb8a675deefd2c41fcf07794100643f999e0c8a
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-26 18:49:05 +0200

    Sidebar + SidebarOption MUI

--- spotify/src/SidebarOptions.js
+++ spotify/src/SidebarOptions.js
@@ -5,7 +5,8 @@ import './SidebarOptions.css'
 function SidebarOptions({title, Icon}) {
     return (
         <div className="sidebarOptions">
-            <p>{title}</p>
+            {Icon && <Icon className="sidebarOptions__icon" />}
+            {Icon ? <h4>{title} </h4> : <p>{title}</p>}
         </div>
     )
 }
