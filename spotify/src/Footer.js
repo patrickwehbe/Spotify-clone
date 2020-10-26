750e8b9bde50079d6bace048e6b99001b040c719
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-26 19:31:29 +0200

    footer final

--- spotify/src/Footer.js
+++ spotify/src/Footer.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline'
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious'
+import SkipNextIcon from '@material-ui/icons/SkipNext'
+import ShuffleIcon from '@material-ui/icons/Shuffle'
+import RepeatIcon from '@material-ui/icons/Repeat'
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay'
+import VolumeDownIcon from '@material-ui/icons/VolumeDown'
+import Grid from '@material-ui/core/Grid'
+import Slider from '@material-ui/core/Slider'
 import './Footer.css'
 
 function Footer() {
     return (
         <div className="footer" >
-            
+            <div className="footer__left"><img src="" alt=""/>
+            <div className="footer__songInfo">
+                <h4>Yeah!</h4>
+                <p>Usher</p>
+                </div>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+                
+                </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
